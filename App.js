0d4b261639b9d9afdfd92d0eadbe37234f8bc29e
Emilin Treesa Joseph <emilintresia02@gmail.com>
2023-01-10 20:05:08 +0530

    First Commit

---
+++ App.js
@@ -0,0 +1,126 @@
+import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native'
+import React, { useState } from 'react'
+import AsyncStorage from '@react-native-async-storage/async-storage'
+
+export default App = () => {
+
+    const [todo, setTodo] = useState("");
+    const [val, setVal] = useState("");
+
+  const insertItem = async () => {
+    try {
+      await AsyncStorage.setItem('key1', todo);
+      alert('Item saved successfully');
+    } catch (e) {
+      alert('Error saving data');
+    }
+  }
+
+  const getItems = async () => {
+
+    try{
+
+        const data = await AsyncStorage.getItem('key1');
+        if(data !== null){
+            setVal(data);
+            setTodo("")
+        } else{
+          alert('No data found');
+        }
+
+    }catch(e){
+      alert('Error getting data');
+    }
+
+
+
+  }
+
+  const clear = async () => {
+    try{
+
+        await AsyncStorage.clear();
+        setVal("")
+
+    }catch(err){
+      alert('Error clearing data');
+    }
+    }
+  
+ 
+  return (
+    <View style={styles.container}>
+      <View style={{alignItems:'center'}}>
+
+      <Text style={{ fontSize: 25,color:'blue' }}>Today's Task</Text>
+      <Text style={{ fontSize: 27,color:'green',marginTop:10 }}>{val}</Text>
+
+      </View>
+      <View>
+
+      <TextInput
+        placeholder='Enter Task...'
+        style={styles.input}
+        onChangeText={text => setTodo(text)}
+        value={todo}
+        />
+      <TouchableOpacity style={styles.btn}
+        onPress={() => insertItem()}
+        >
+        <Text style={styles.text}>ADD</Text>
+
+      </TouchableOpacity>
+
+      <TouchableOpacity style={styles.btn}
+        onPress={() =>getItems()}
+      >
+
+        <Text style={styles.text}>GET</Text>
+
+      </TouchableOpacity>
+      <TouchableOpacity style={styles.btn}
+        onPress={() =>clear()}
+      >
+
+        <Text style={styles.text}>CLEAR</Text>
+
+      </TouchableOpacity>
+
+        </View>
+    </View>
+  )
+  }
+
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'space-around',
+  },
+  btn: {
+    width: 200,
+    height: 40,
+    backgroundColor: 'grey',
+    padding: 5,
+    marginTop: 10,
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 5,
+  },
+  text: {
+    color: 'white',
+    fontSize: 18
+  },
+  input: {
+    width: 200,
+    height: 60,
+    borderBottomColor: 'black',
+    borderBottomWidth: 1,
+    padding: 5
+
+  }
+})
